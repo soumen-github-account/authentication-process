608a44270b5a1617ea4eadd83ba971e8f6a09cfa
soumen-github-account <soumendas7584@gmail.com>
2026-03-27 20:40:09 +0530

    second-commit

---
+++ node-auth/server.js
@@ -0,0 +1,14 @@
+
+
+import express from 'express'
+
+const app = express()
+const port = 8000;
+
+app.get('/', (req, res)=>{
+    res.send("Api is working...")
+})
+
+app.listen(port, ()=>{
+    console.log("App started")
+})
